34c8b92c084840fc50a66f22b6a82f509ca02036
OZR0107 <zhuo0107@foxmail.com>
2025-06-23 11:20:59 +0800

    新增 safelist 配置以支持 link 类

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+  safelist: [
+    'link', // 添加 link 类
+  ],
   darkMode: "class", // allows toggling dark mode manually
   theme: {
     extend: {
